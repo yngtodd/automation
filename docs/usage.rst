54db18a4485aace2e7b7d89e91f84cc5830c438b
yngtodd <young.todd.mk@gmail.com>
2019-02-10 15:04:43 -0500

    :octocat:

---
+++ docs/usage.rst
@@ -0,0 +1,11 @@
+.. highlight:: shell
+
+=====
+Usage
+=====
+
+To use Automation in a project:
+
+.. code-block:: console
+
+    import automation
---
+++ test/test_automation.py
@@ -0,0 +1,19 @@
+"""
+Tests for `automation` module.
+"""
+import pytest
+from automation import automation
+
+
+class TestAutomation(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_something(self):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
